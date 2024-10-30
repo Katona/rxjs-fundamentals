eebe128e48e1ac57b7cc54040ce655ef8a0d2ce4
ghornyak_gartner <gabor.hornyak@gartner.com>
2024-10-30 17:17:18 +0100

    feat: basic-counter

--- applications/basic-counter/script.ts
+++ applications/basic-counter/script.ts
@@ -1,5 +1,16 @@
-import { fromEvent, interval, merge, NEVER } from 'rxjs';
+import { fromEvent, interval, merge, NEVER, Subscription, timer } from 'rxjs';
 import { setCount, startButton, pauseButton } from './utilities';
 
 const start$ = fromEvent(startButton, 'click');
 const pause$ = fromEvent(pauseButton, 'click');
+
+// const interval$ = interval(1000);
+const interval$ = timer(1000, 800); //, 1000);
+
+let currentIntervalSubscription: Subscription | undefined;
+
+start$.subscribe(() => {
+  if (currentIntervalSubscription) currentIntervalSubscription.unsubscribe();
+  currentIntervalSubscription = interval$.subscribe((v) => setCount(v + 1));
+});
+pause$.subscribe(() => currentIntervalSubscription?.unsubscribe());
