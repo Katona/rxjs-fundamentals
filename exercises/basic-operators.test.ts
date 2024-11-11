a9d9f9ae728f982a8f11b4dcb7898b407697503b
ghornyak_gartner <gabor.hornyak@gartner.com>
2024-11-11 14:32:19 +0100

    feat: migrate tests to Typescript

--- exercises/basic-operators.test.ts
+++ exercises/basic-operators.test.ts
@@ -1,4 +1,4 @@
-import { from, of } from 'rxjs';
+import { from, Observable, of } from 'rxjs';
 import {
   take,
   skip,
@@ -15,23 +15,26 @@ import { fibonacci } from '../utilities/fibonacci';
  * Returns all of the values emitted by an observable as an array.
  * I'm tired of typing this out for every test.
  */
-const getResult = async (observable) => {
+const getResult = async <T>(observable: Observable<T>) => {
   return new Promise((resolve, reject) => {
-    const result = [];
+    const result: T[] = [];
     const subscription = observable.subscribe({
       next: (value) => result.push(value),
       error: reject,
       complete: () => {
         resolve(result);
-        subscription.unsubscribe();
+        // subscription.unsubscribe();
       },
     });
   });
 };
 
 describe('Basic Operators', () => {
-  it.skip('should take the first 5 values and map them to the word "DINOSAUR"', async () => {
-    const observable$ = of(1, 2, 3, 4, 5, 6, 7).pipe();
+  it('should take the first 5 values and map them to the word "DINOSAUR"', async () => {
+    const observable$ = of(1, 2, 3, 4, 5, 6, 7).pipe(
+      take(5),
+      mapTo('DINOSAUR'),
+    );
 
     return expect(await getResult(observable$)).toEqual([
       'DINOSAUR',
@@ -43,19 +46,28 @@ describe('Basic Operators', () => {
   });
 
   it.skip('should skip the first 5 values and double last two', async () => {
-    const observable$ = of(1, 2, 3, 4, 5, 6, 7).pipe();
+    const observable$ = of(1, 2, 3, 4, 5, 6, 7).pipe(
+      skip(5),
+      map((x) => x * 2),
+    );
 
     return expect(await getResult(observable$)).toEqual([12, 14]);
   });
 
   it.skip('should emit the square of every even number in the stream', async () => {
-    const observable$ = of(1, 2, 3, 4, 5, 6, 7).pipe();
+    const observable$ = of(1, 2, 3, 4, 5, 6, 7).pipe(
+      filter((x) => x % 2 === 0),
+      map((x) => x * 2),
+    );
 
     return expect(await getResult(observable$)).toEqual([4, 16, 36]);
   });
 
   it.skip('should sum of the total of all of the Fibonacci numbers under 200', async () => {
-    const observable$ = from(fibonacci()).pipe();
+    const observable$ = from(fibonacci()).pipe(
+      takeWhile((x) => x < 200),
+      reduce((sum, x) => sum * x),
+    );
 
     expect(await getResult(observable$)).toEqual([376]);
   });
