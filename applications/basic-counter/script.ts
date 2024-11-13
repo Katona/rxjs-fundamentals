e906e36d65b77f23de11776b4d01dce5261c5f64
ghornyak_gartner <gabor.hornyak@gartner.com>
2024-11-13 15:40:13 +0100

    feat: improve counter

--- applications/basic-counter/script.ts
+++ applications/basic-counter/script.ts
@@ -1,16 +1,23 @@
-import { fromEvent, interval, merge, NEVER, Subscription, timer } from 'rxjs';
+import {
+  fromEvent,
+  interval,
+  merge,
+  NEVER,
+  scan,
+  skipUntil,
+  Subscription,
+  takeUntil,
+  timer,
+} from 'rxjs';
 import { setCount, startButton, pauseButton } from './utilities';
 
 const start$ = fromEvent(startButton, 'click');
 const pause$ = fromEvent(pauseButton, 'click');
 
-// const interval$ = interval(1000);
-const interval$ = timer(1000, 800); //, 1000);
-
-let currentIntervalSubscription: Subscription | undefined;
-
-start$.subscribe(() => {
-  if (currentIntervalSubscription) currentIntervalSubscription.unsubscribe();
-  currentIntervalSubscription = interval$.subscribe((v) => setCount(v + 1));
-});
-pause$.subscribe(() => currentIntervalSubscription?.unsubscribe());
+const interval$ = interval(100)
+  .pipe(
+    skipUntil(start$),
+    scan((v) => v + 1, 0),
+    takeUntil(pause$),
+  )
+  .subscribe(setCount);
