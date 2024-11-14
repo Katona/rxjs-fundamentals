9f7e11fafbd0e6a18e4d90fae7139640e1754186
ghornyak_gartner <gabor.hornyak@gartner.com>
2024-11-14 11:39:57 +0100

    feat: merging-timelines

--- applications/merging-timelines/script.ts
+++ applications/merging-timelines/script.ts
@@ -7,3 +7,19 @@ import {
   setStatus,
   bootstrap,
 } from './utilities';
+
+if (startButton && pauseButton) {
+  const startEvent$ = fromEvent(startButton, 'click').pipe(mapTo(true));
+  const pauseEvent$ = fromEvent(pauseButton, 'click').pipe(mapTo(false));
+  const merged$ = merge(startEvent$, pauseEvent$).pipe(startWith(false));
+  merged$.subscribe(setStatus);
+}
+const first$ = interval(1000).pipe(map(labelWith('First')), take(4));
+const second$ = interval(1000).pipe(map(labelWith('Second')), take(4));
+// const combined$ = interval(1000).pipe(map(labelWith('Combined')), take(4));
+// const combined$ = merge(first$, second$);
+// const combined$ = concat(first$, second$);
+// const combined$ = race(first$, second$);
+const combined$ = forkJoin(first$, second$);
+
+bootstrap({ first$, second$, combined$ });
