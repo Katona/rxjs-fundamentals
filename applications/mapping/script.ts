84927cdfbc4527fabaca28209e89073c555ad91f
ghornyak_gartner <gabor.hornyak@gartner.com>
2024-11-15 15:35:40 +0100

    feat: mapping

--- applications/mapping/script.ts
+++ applications/mapping/script.ts
@@ -1,5 +1,14 @@
 import { of, from, interval, fromEvent, merge, NEVER } from 'rxjs';
-import { pluck, concatMap, take, map } from 'rxjs/operators';
+import {
+  pluck,
+  concatMap,
+  take,
+  map,
+  mergeMap,
+  scan,
+  takeUntil,
+  skipUntil,
+} from 'rxjs/operators';
 
 import {
   getCharacter,
@@ -9,6 +18,17 @@ import {
   setStatus,
 } from './utilities';
 
-const character$ = from(getCharacter(1)).pipe(pluck('name'));
+if (startButton && pauseButton) {
+  const startEvents = fromEvent(startButton, 'click');
+  const pauseEvents = fromEvent(pauseButton, 'click');
+  const character$ = interval(1000)
+    .pipe(
+      skipUntil(startEvents),
+      takeUntil(pauseEvents),
+      scan((n: number) => n + 1, 1),
+    )
+    .pipe(mergeMap((n) => from(getCharacter(n))))
+    .pipe(pluck('name'));
 
-character$.subscribe(render);
+  character$.subscribe(render);
+}
