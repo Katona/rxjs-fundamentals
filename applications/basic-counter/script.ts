9fc30d6ae202f9720027eb4164ad7137ccd0515c
ghornyak_gartner <gabor.hornyak@gartner.com>
2024-11-18 06:40:12 +0100

    feat: improve basic-counter with `switchMap`

--- applications/basic-counter/script.ts
+++ applications/basic-counter/script.ts
@@ -1,23 +1,36 @@
 import {
   fromEvent,
   interval,
+  mapTo,
   merge,
   NEVER,
   scan,
+  shareReplay,
   skipUntil,
+  startWith,
   Subscription,
+  switchMap,
   takeUntil,
   timer,
 } from 'rxjs';
 import { setCount, startButton, pauseButton } from './utilities';
 
-const start$ = fromEvent(startButton, 'click');
-const pause$ = fromEvent(pauseButton, 'click');
+const start$ = fromEvent(startButton, 'click').pipe(mapTo(true));
+const pause$ = fromEvent(pauseButton, 'click').pipe(mapTo(false));
 
-const interval$ = interval(100)
+const counter = interval(1000).pipe(shareReplay());
+
+merge(start$, pause$)
   .pipe(
-    skipUntil(start$),
-    scan((v) => v + 1, 0),
-    takeUntil(pause$),
+    startWith(false),
+    switchMap((isRunning) => (isRunning ? counter : NEVER)),
   )
   .subscribe(setCount);
+
+// interval(100)
+//   .pipe(
+//     skipUntil(start$),
+//     scan((v) => v + 1, 0),
+//     takeUntil(pause$),
+//   )
+//   .subscribe(setCount);
