eeaa038dd0d6fcf2b826c544c70b72cc1205f6be
ghornyak_gartner <gabor.hornyak@gartner.com>
2024-11-22 13:54:05 +0100

    feat: dog-facts

--- applications/dog-facts/script.ts
+++ applications/dog-facts/script.ts
@@ -1,4 +1,4 @@
-import { fromEvent, of, timer, merge, NEVER } from 'rxjs';
+import { fromEvent, of, timer, merge, NEVER, throwError, EMPTY } from 'rxjs';
 import { fromFetch } from 'rxjs/fetch';
 import {
   catchError,
@@ -10,6 +10,8 @@ import {
   switchMap,
   tap,
   pluck,
+  map,
+  exhaustAll,
 } from 'rxjs/operators';
 
 import {
@@ -21,4 +23,38 @@ import {
   setError,
 } from './utilities';
 
-const endpoint = 'http://localhost:3333/api/facts';
+// const endpoint = 'http://localhost:3333/api/facts';
+const endpoint =
+  'http://localhost:3333/api/facts?delay=3000&chaos=1&flakiness=20';
+
+const fetchData = () => {
+  console.log('fetchData');
+  return fromFetch(endpoint).pipe(
+    mergeMap((res) => {
+      console.log({ ok: res.ok });
+      if (res.ok) return res.json();
+      throw new Error('Fetch failed');
+    }),
+    retry(4),
+    catchError((error) => {
+      return of(error.message);
+    }),
+  );
+};
+
+if (fetchButton && stopButton) {
+  const fetchClick$ = fromEvent(fetchButton, 'click').pipe(mapTo(true));
+  const pauseClick$ = fromEvent(stopButton, 'click').pipe(mapTo(false));
+  const factStream$ = merge(fetchClick$, pauseClick$).pipe(
+    switchMap((shouldFetch) => {
+      if (shouldFetch) return timer(0, 5000).pipe(map(fetchData));
+      else return NEVER;
+    }),
+    exhaustAll(),
+    tap(() => clearError()),
+    tap(() => clearFacts()),
+  );
+  // const fetches = fromEvent(fetchButton, 'click').pipe(exhaustMap(fetchData));
+
+  factStream$.subscribe(addFacts);
+}
