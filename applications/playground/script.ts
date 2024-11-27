b6c373b67151fb611c28eae690314b2e1e7f1549
ghornyak_gartner <gabor.hornyak@gartner.com>
2024-11-27 22:57:20 +0100

    feat: playground

--- applications/playground/script.ts
+++ applications/playground/script.ts
@@ -1,4 +1,18 @@
-import { of } from 'rxjs';
+import { catchError, of, pluck, retry, switchMap } from 'rxjs';
+import { fromFetch } from 'rxjs/fetch';
 import './playground';
 
-export const example$ = of(1, 2, 3, 4);
+const endpoint = 'http://localhost:3333/api/pokemon?flakiness=3&delay=5000';
+
+export const example$ = fromFetch(endpoint).pipe(
+  switchMap((res) => {
+    if (res.ok) return res.json();
+    else throw new Error('Failed to fetch');
+  }),
+  retry(4),
+  catchError((error) => {
+    console.error(error.message);
+    return of({ error: true, message: error.message });
+  }),
+  pluck('pokemon'),
+);
