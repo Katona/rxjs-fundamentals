3edd62fbf5487b5bb33e64ee334e29c78a5bea48
ghornyak_gartner <gabor.hornyak@gartner.com>
2024-12-04 23:37:27 +0100

    feat: loading data over time

--- applications/pokemon/script.ts
+++ applications/pokemon/script.ts
@@ -15,6 +15,8 @@ import {
   take,
   EMPTY,
   pluck,
+  identity,
+  takeLast,
 } from 'rxjs';
 
 import { fromFetch } from 'rxjs/fetch';
@@ -26,10 +28,13 @@ import {
   endpointFor,
   search,
   form,
+  submit,
+  renderPokemon,
 } from '../pokemon/utilities';
 
 const endpoint = 'http://localhost:3333/api/pokemon/search';
 
+type Pokemon = { name: string; id: string };
 if (search) {
   const search$ = fromEvent(search, 'input').pipe(
     debounceTime(300),
@@ -44,4 +49,26 @@ if (search) {
     tap(addResults),
   );
   search$.subscribe();
+}
+
+if (submit) {
+  const submit$ = fromEvent(submit, 'click').pipe(
+    mergeMap(() =>
+      fromFetch(`${endpoint}/${search.value}?delay=1000&chaos=true`).pipe(
+        switchMap((res) => res.json()),
+      ),
+    ),
+    map((json): Pokemon[] => json.pokemon),
+    mergeMap(identity),
+    take(1),
+    switchMap((pokemon: Pokemon) => {
+      const pokemonWithData$ = fromFetch(endpointFor(pokemon.id)).pipe(
+        mergeMap((res) => res.json()),
+        map((json) => ({ ...pokemon, data: json })),
+      );
+      return merge(of(pokemon), pokemonWithData$);
+    }),
+    tap((pokemon) => renderPokemon(pokemon)),
+  );
+  submit$.subscribe();
 }
