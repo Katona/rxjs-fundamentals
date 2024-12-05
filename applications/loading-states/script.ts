37a75f6199ff8750004f40756327012d075fd82d
ghornyak_gartner <gabor.hornyak@gartner.com>
2024-12-05 10:56:18 +0100

    feat: loading-states

--- applications/loading-states/script.ts
+++ applications/loading-states/script.ts
@@ -1,12 +1,30 @@
-import { fromEvent, concat, of, race, timer } from 'rxjs';
-import { tap, exhaustMap, delay, shareReplay, first } from 'rxjs/operators';
+import { concat, fromEvent, race, timer } from 'rxjs';
+import { exhaustMap, shareReplay, tap } from 'rxjs/operators';
 
 import {
-  responseTimeField,
+  fetchData,
+  form,
+  showLoading,
   showLoadingAfterField,
   showLoadingForAtLeastField,
-  loadingStatus,
-  showLoading,
-  form,
-  fetchData,
 } from './utilities';
+
+const fetch$ = fromEvent(form, 'submit').pipe(
+  exhaustMap(() => {
+    const fetchData$ = fetchData().pipe(shareReplay(1));
+    const shouldShowLoading$ = timer(
+      Number.parseInt(showLoadingAfterField.value),
+    ).pipe(tap(() => showLoading(true)));
+    const hideLoading$ = timer(
+      Number.parseInt(showLoadingForAtLeastField.value),
+    );
+    const fetchWithLoading$ = concat(
+      shouldShowLoading$,
+      hideLoading$,
+      fetchData$.pipe(tap(() => showLoading(false))),
+    );
+    return race(fetchData$, fetchWithLoading$);
+  }),
+);
+
+fetch$.subscribe(console.log);
